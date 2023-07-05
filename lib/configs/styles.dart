c9a65389f70dc06947f15dfb997dffefe219f675
Harun Nurahman <harun.nurrahman@hotmail.com>
2023-07-05 15:44:17 +0700

    Adding Styles

---
+++ lib/configs/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Basic Color
+Color blackColor = const Color(0xFF000000);
+Color blackAccent = const Color(0xFF212244);
+Color greyColor = const Color(0xFF94959B);
+Color lineDark = const Color(0xFFEAEAEA);
+Color whiteGrey = const Color(0xFFF1F1F5);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color redColor = const Color(0xFFFF314A);
+Color blueColor = const Color(0xFF2972FF);
+Color yellowColor = const Color(0xFFFFBA00);
+
+// Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+// Text
